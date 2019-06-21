278707f96be8432f5c456440a66278199a65d7ae
John <johlee@g.hmc.edu>
2019-06-21 10:32:47 -0700

    finish teleop, plus rosparam for simulation

--- motor_control/setup.py
+++ motor_control/setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-from disutils.core import setup
+from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-setup_args = generate_disutils_setup(
-        package=['motor_control']
-        package_dir={'': 'src'}
+setup_args = generate_distutils_setup(
+    packages=['motor_control'],
+    package_dir={'': 'src'}
 )
 
 setup(**setup_args)
--- motor_control/src/motor_control/motor_serial.py
+++ motor_control/src/motor_control/motor_serial.py
@@ -3,6 +3,9 @@
 import serial
 import rospy
 from motor_control.msg import MotorCommand
+from geometry_msgs.msg import Twist
+
+
 strboard_ser = None
 port_ser = None
 
@@ -12,12 +15,40 @@ def setup_serial():
     port_ser = serial.Serial('/dev/serial/by-id/usb-FTDI_USB_Serial_Converter_FT8VWDWP-if00-port0', 115200)
     strboard_ser = serial.Serial('/dev/serial/by-id/usb-FTDI_USB_Serial_Converter_FT8VW9AR-if00-port0', 115200)
 
-def send_cmd_callaback(data):
+def send_cmd_callback(data):
     '''send command to motor everytime we recieved a callback'''
-    
+    # motor value is between -1000 to 1000
+
+def teleop_callback(data):
+    '''send command to motor with teleop'''
+
+    # TODO: calibrate the speed to pwm
+    forward = min(1000, max(-1000, data.linear.x * 100))
+    turn = min(1800, max(1200, data.angular.z*200 + 1600))
+
+    update_cmd(forward, forward, turn)
+
+def update_cmd(port, starboard, servo):
+    '''send command to motor'''
+    global port_ser, strboard_ser
+
+    if rospy.get_param('/sim'):
+        print('Port, Starboard, Servo', port, starboard, servo)
+    else:
+        port_command = '!G 1 %d' % port
+        starboard_command = '!G 1 %d' % starboard
+        port_ser.write(port_command.encode() + + b'\r\n')
+        strboard_ser.write(starboard_command + b'\r\n')
 
 def main():
+    sim = rospy.get_param('/sim')
+
+    if sim == True:
+        rospy.loginfo('YEET')
+    else:
+        setup_serial()
+
     rospy.init_node('motor_controller')
     rospy.Subscriber('motor_cmd_reciever', MotorCommand, send_cmd_callback)
-
+    rospy.Subscriber('cmd_vel', Twist, teleop_callback)
     rospy.spin()
